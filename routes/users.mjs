2d87943535621ca8b894d2106237084b7c47630d
Alfredo González <alfredo.arosteguy@gmail.com>
2022-01-01 19:58:12 -0300

    WIP: Add /users POST method

--- routes/users.mjs
+++ routes/users.mjs
@@ -4,12 +4,27 @@ export default (app) => {
   app.get('/users', (req, res, next) => {
     axios('https://jsonplaceholder.typicode.com/users')
       .then(response => res.status(200).json(response.data))
-      .catch(next);
+      .catch(error => next(error.response.status));
+  });
+
+  app.post('/users', (req, res) => {
+    axios.post('https://jsonplaceholder.typicode.com/users',
+    JSON.stringify({
+        name: 'A name',
+        email: 'aname@anemail.com'
+      })
+      ,{
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      })
+      .then(response => res.status(response.status).json(response.data))
+      .catch(error => next(error.response.status));
   });
 
   app.get('/users/:userId', (req, res, next) => {
     axios('https://jsonplaceholder.typicode.com/users/'+req.params.userId)
       .then(response => res.status(200).json(response.data))
-      .catch(err => next(err.response.status));
+      .catch(error => next(error.response.status));
   });
 }
