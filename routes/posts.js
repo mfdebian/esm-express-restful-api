053d536512e8a1e0c6a13b1c1a29040968006952
Alfredo González <alfredo.arosteguy@gmail.com>
2024-08-04 21:31:21 -0400

    Changed to async/await syntax

--- routes/posts.js
+++ routes/posts.js
@@ -1,55 +1,83 @@
 export default (app) => {
-  app.get('/posts', (req, res, next) => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
-  });
-  
-  app.get('/posts/:postId', (req, res, next) => {
-    fetch('https://jsonplaceholder.typicode.com/posts/'+req.params.postId)
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+  app.get('/posts', async (req, res, next) => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.post('/posts', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
-
-    fetch('https://jsonplaceholder.typicode.com/posts/', {
-      method: "POST",
-      body: JSON.stringify(req.body),
-      headers,
-    })
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+  app.get('/posts/:postId', async (req, res, next) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${req.params.postId}`
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.put('/posts/:postId', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
+  app.post('/posts', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
 
-    fetch('https://jsonplaceholder.typicode.com/posts/'+req.params.postId, {
-      method: "PUT",
-      body: JSON.stringify(req.body),
-      headers,
-    })
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts/',
+        {
+          method: 'POST',
+          body: JSON.stringify(req.body),
+          headers,
+        }
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.delete('/posts/:postId', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
+  app.put('/posts/:postId', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
 
-    fetch('https://jsonplaceholder.typicode.com/posts/'+req.params.postId, {
-      method: "DELETE",
-      headers,
-    })
-      .then(data => res.status(200).json(data.statusText))
-      .catch(error => next(error.status));
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${req.params.postId}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(req.body),
+          headers,
+        }
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
-}
+
+  app.delete('/posts/:postId', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
+
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${req.params.postId}`,
+        {
+          method: 'DELETE',
+          headers,
+        }
+      );
+      res.status(200).json(response.statusText);
+    } catch (error) {
+      next(error.status);
+    }
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,83 @@
 export default (app) => {
-  app.get('/users', (req, res, next) => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
-  });
-  
-  app.get('/users/:userId', (req, res, next) => {
-    fetch('https://jsonplaceholder.typicode.com/users/'+req.params.userId)
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+  app.get('/users', async (req, res, next) => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.post('/users', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
-
-    fetch('https://jsonplaceholder.typicode.com/users/', {
-      method: "POST",
-      body: JSON.stringify(req.body),
-      headers,
-    })
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+  app.get('/users/:userId', async (req, res, next) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${req.params.userId}`
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.put('/users/:userId', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
+  app.post('/users', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
 
-    fetch('https://jsonplaceholder.typicode.com/users/'+req.params.userId, {
-      method: "PUT",
-      body: JSON.stringify(req.body),
-      headers,
-    })
-      .then(res => res.json())
-      .then(data => res.status(200).send(data))
-      .catch(error => next(error.status));
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users/',
+        {
+          method: 'POST',
+          body: JSON.stringify(req.body),
+          headers,
+        }
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
 
-  app.delete('/users/:userId', (req, res, next) => {
-    const headers = new Headers();
-    headers.append('Content-Type', req.get('Content-Type'));
+  app.put('/users/:userId', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
 
-    fetch('https://jsonplaceholder.typicode.com/users/'+req.params.userId, {
-      method: "DELETE",
-      headers,
-    })
-      .then(data => res.status(200).json(data.statusText))
-      .catch(error => next(error.status));
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${req.params.userId}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(req.body),
+          headers,
+        }
+      );
+      const data = await response.json();
+      res.status(200).send(data);
+    } catch (error) {
+      next(error.status);
+    }
   });
-}
+
+  app.delete('/users/:userId', async (req, res, next) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Type', req.get('Content-Type'));
+
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${req.params.userId}`,
+        {
+          method: 'DELETE',
+          headers,
+        }
+      );
+      res.status(200).json(response.statusText);
+    } catch (error) {
+      next(error.status);
+    }
+  });
+};
